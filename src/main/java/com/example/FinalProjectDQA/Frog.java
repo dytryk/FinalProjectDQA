3a2671222e44272230117e01b82487b5b44d3153
dytryk <astonmartinone111111@gmail.com>
2022-05-04 22:50:43 -0400

    movements

--- src/main/java/com/example/FinalProjectDQA/Frog.java
+++ src/main/java/com/example/FinalProjectDQA/Frog.java
@@ -5,7 +5,7 @@ public class Frog extends Unit {
      * this class converts any attached unit to Frog
      */
     @Override
-   public Unit convert(){
-       return new Frog();
-   }
+    public Unit convert(){
+        return new Frog();
+    }
 }
--- src/main/java/com/example/FinalProjectDQA/Unit.java
+++ src/main/java/com/example/FinalProjectDQA/Unit.java
@@ -2,27 +2,12 @@ package com.example.FinalProjectDQA;
 
 public abstract class Unit {
     /**
-     * this class moves and locates unit objects, and defines units as either a frog, space, obstacle
+     * this class defines units as either a frog, space, or obstacle
      */
 
     /**
      * converts a number to the corresponding character to display either frog, space, or obstacle
-     *
      * @return char corresponding to user input for UI
      */
     public abstract Unit convert();
-
-    /**
-     * changes a Unit value to the corresponding char
-     * @return char corresponding to unit type
-     */
-    public char toChar() {
-        if (this.getClass().equals(Obstacle.class)){
-            return 'X';
-        }
-        if (this.getClass().equals(Frog.class)){
-            return 'O';
-        }
-        return ' ';
-    }
 }
